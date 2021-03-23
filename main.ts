672778d5487cc4461126fa7afccb6e9b205a7c72
ha-dida <hdruoff@gmx.de>
2021-03-23 23:11:19 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     music.playTone(262, music.beat(BeatFraction.Sixteenth))
+    spaltenwert += 0
     bearbeite_spalte += 1
-    spaltenwert += 1
     if (bearbeite_spalte > 4) {
-        spaltenwert = 0
+        bearbeite_spalte = 0
+    }
+    for (let index = 0; index <= 4; index++) {
+        led.toggle(bearbeite_spalte, index)
+        for (let index2 = 0; index2 < 3; index2++) {
+            led.toggle(bearbeite_spalte, spaltenwert)
+            basic.pause(100)
+            led.plot(bearbeite_spalte, spaltenwert)
+            basic.pause(100)
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -17,8 +26,8 @@ input.onButtonPressed(Button.B, function () {
 })
 let loesung_der_spalte = 0
 let code_richtig = 0
-let spaltenwert = 0
 let bearbeite_spalte = 0
+let spaltenwert = 0
 let loesung = 12043
 led.plot(0, 0)
 basic.setLedColor(0xff0000)
@@ -26,9 +35,18 @@ music.setTempo(200)
 basic.forever(function () {
     code_richtig = 0
     for (let spalte = 0; spalte <= 4; spalte++) {
-        loesung_der_spalte = parseFloat("123")
+        loesung_der_spalte = parseFloat(loesung.charAt(spalte))
         for (let zeile = 0; zeile <= 4; zeile++) {
-        	
+            if (led.point(spalte, zeile)) {
+                if (zeile == loesung_der_spalte) {
+                    code_richtig += 1
+                }
+            }
         }
+    }
+    if (code_richtig == 5) {
+        basic.setLedColor(0x00ff00)
+    } else {
+        basic.setLedColor(0xff0000)
     }
 })
